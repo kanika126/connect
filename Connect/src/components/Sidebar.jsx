58bee8110d18386e8ed945220ef3b7a239b01c4d
KANIKA <105192607+kanika126@users.noreply.github.com>
2023-12-14 22:53:54 +0530

    .

--- Connect/src/components/Sidebar.jsx
+++ Connect/src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
-    <div className="bg-gray-800 text-white h-full p-4">
+    <div className="bg-indigo-500 text-white h-full p-4">
       <h2 className="text-2xl font-bold mb-4">Approval Options</h2>
       <ul>
         <li>
