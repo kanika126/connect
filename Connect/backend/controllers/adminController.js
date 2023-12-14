8f9260f06af6fd3fb8e828f4a9d009db1ea9a815
NIKHIL GUPTA <nikhil20530@iiitd.ac.in>
2023-12-14 12:09:33 +0530

    admin changes

--- Connect/backend/controllers/adminController.js
+++ Connect/backend/controllers/adminController.js
@@ -1,5 +1,9 @@
-const Admin = require('../models/Admin');
 
+const bcrypt = require('bcrypt');
+const Admin = require('../models/Admin');
+const CollegeExperience= require('../models/CollegeExperience')
+const WorkExperience= require('../models/WorkExperience')
+const Resume= require('../models/Resume')
 const getAdmins = async (req, res) => {
   try {
     const admins = await Admin.find();
@@ -9,12 +13,18 @@ const getAdmins = async (req, res) => {
   }
 };
 
+
 const addAdmin = async (req, res) => {
   try {
     const { email, username, password } = req.body;
 
-    // Placeholder logic: Validate input and create a new admin
-    const newAdmin = new Admin({ email, username, password });
+    // Hash the password before saving
+    const hashedPassword = await bcrypt.hash(password, 10); // 10 is the number of salt rounds
+
+    // Create a new admin with the hashed password
+    const newAdmin = new Admin({ email, username, password: hashedPassword });
+    
+    // Save the admin to the database
     const savedAdmin = await newAdmin.save();
 
     res.status(201).json(savedAdmin);
@@ -22,6 +32,9 @@ const addAdmin = async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 };
+
+module.exports = { addAdmin };
+
 
 const getAdmin = async (req, res) => {
   try {
@@ -70,4 +83,51 @@ const deleteAdmin = async (req, res) => {
   }
 };
 
-module.exports = { getAdmins, addAdmin, getAdmin, updateAdmin, deleteAdmin };
+
+// Generic approval API
+const approveExperience = async (req, res) => {
+  const { experienceId } = req.params;
+  const { type } = req.body; // Extract the experience type from the payload
+
+  try {
+    let updatedExperience;
+
+    // Handle different types of experiences
+    switch (type) {
+      case 'college':
+        updatedExperience = await CollegeExperience.findByIdAndUpdate(
+          experienceId,
+          { approved: true },
+          { new: true }
+        );
+        break;
+      case 'work':
+        updatedExperience = await WorkExperience.findByIdAndUpdate(
+          experienceId,
+          { approved: true },
+          { new: true }
+        );
+        break;
+      case 'resume':
+        updatedExperience = await Resume.findByIdAndUpdate(
+          experienceId,
+          { approved: true },
+          { new: true }
+        );
+        break;
+      default:
+        return res.status(400).json({ message: 'Invalid experience type' });
+    }
+
+    if (!updatedExperience) {
+      return res.status(404).json({ message: `${type} experience not found` });
+    }
+
+    res.json(updatedExperience);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+
+module.exports = { approveExperience,getAdmins, addAdmin, getAdmin, updateAdmin, deleteAdmin };
